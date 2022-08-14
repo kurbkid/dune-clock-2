eb4c4a558326be1e602d843ca3fff9970752466d
Dick van den Broek <thedickrulez@hotmail.com>
2022-08-14 20:02:35 +0200

    Managed with Yarn this time

--- App.js
+++ App.js
@@ -1,20 +1,133 @@
+import React, {useState} from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { 
+  SafeAreaView,
+  StyleSheet, 
+  Text, 
+  View,
+  ImageBackground,
+} from 'react-native';
+import {Picker} from '@react-native-picker/picker';
+import ChessClocks from './ChessClocks';
 
-export default function App() {
+import bckgrnd from './assets/duneboxcut2.png'; 
+
+//the player colors eyedropped from dune and their names. 
+const colors = ['#89292A','#9B6F34','#1E3560','#186047']
+const colornames = ['red','yellow','blue','green']
+
+
+const App = () => {
+  const [numberOfPlayers,setNumberOfPlayers] = useState(4);
+  const [timerDuration, setTimerDuration] = useState(25*60);
+  const [playerColors, setPlayerColors] = useState(colors);
+  
+  //fill arrays with items for the Pickers
+  const timeItems = []
+  for (i=1; i<46; i++){ timeItems.push(<Picker.Item key={i} label={String(i)} value={i*60}/>)}
+  const numberOfPlayersItems = []
+  for (i=1; i<5; i++){ numberOfPlayersItems.push(<Picker.Item key={i} label={String(i)} value={i}/>)}
+  const colorItems = []
+  for (i=0; i<4; i++){ colorItems.push(<Picker.Item key={i} label={colornames[i]} value={colors[i]}/>)}
+
+  //Color picker for certain player
+  const PlayerColorPicker = (i) => {
+    return(
+      <View key={i} style={{flex:1,flexDirection:'row'}}>
+        <Picker
+          selectedValue={playerColors[i]}
+          style={{flex:1,backgroundColor:playerColors[i]}}
+          onValueChange={(itemValue,itemIndex) => { 
+            let c = playerColors.slice()
+            c[i] = itemValue
+            setPlayerColors(c)
+          }}
+          >
+          {colorItems}
+        </Picker>
+      </View>
+    )
+  }
+  //Array with Color Pickers for each player
+  PlayerColorPickerArray = []
+  for (i=0;i<numberOfPlayers; i++){
+    PlayerColorPickerArray.push(PlayerColorPicker(i))
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={{flex:1}}>
+      <ImageBackground source={bckgrnd} resizeMode="cover" style={styles.background}>
+
+      {/* Top part of the app with the settings  */}
+      <View style={[styles.container, {justifyContent:'flex-end',marginTop:30}]}>
+        {/* Display the number of players and time per player Pickers */}
+        <View style={[styles.container, {flexDirection: "row"}]}>
+          <Picker 
+            selectedValue={numberOfPlayers}
+            style={{flex:1,color:colors[1]}}
+            dropdownIconColor={colors[1]}
+            onValueChange={(itemValue, itemIndex) => {
+              setNumberOfPlayers(itemValue);
+            }}
+          >
+            {numberOfPlayersItems}
+          </Picker>
+          <Text style={[styles.normal, {flex:1}]}>players</Text>
+          <Picker
+            selectedValue={timerDuration}
+            style={{flex:1,color:colors[1]}}
+            dropdownIconColor={colors[1]}
+            onValueChange={(itemValue, itemIndex) => {
+              setTimerDuration(itemValue)
+            }}
+            >
+            {timeItems}
+          </Picker>
+          <Text style={[styles.normal, {flex:1}]}>minutes</Text>
+        </View>    
+      </View>
+      
+      {/* Display the color pickers for each player */}
+      <View style={[styles.container, {flexDirection:'row'}]}>
+        <Text style={[styles.normal, {flex:1}]}>Player colors:</Text>
+        {PlayerColorPickerArray}
+      </View>
+      
+      {/* Main part of the app that displays the clocks */}
+      <View style={{flex:8}}>
+        <ChessClocks numberOfPlayers={numberOfPlayers} duration={timerDuration} colors={playerColors}/>
+      </View>
+  
+      </ImageBackground>
+    </SafeAreaView>
   );
 }
+
+
+export default App;
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+    padding: 0,
     justifyContent: 'center',
+    alignItems: 'center',
+  },
+  normal: {
+    fontSize: 20,
+    color : colors[1],
+  },
+  clock: {
+    flex: 1,
+  },
+  background: {
+    flex: 1,
+    justifyContent: "center"
+  },
+  buttonText: {
+    fontSize: 20,
+    marginTop: 10,
   },
 });
+
+
---
+++ ChessClocks.js
@@ -0,0 +1,98 @@
+import React, {useState} from 'react';
+import { 
+    Text, 
+    View,
+    StyleSheet, 
+    Button,
+    TouchableOpacity,
+  } from 'react-native';
+import { CountdownCircleTimer } from 'react-native-countdown-circle-timer';
+import pSBC from './colorhack'
+
+//some functions to display the time
+const getMinutes = (time) => (Math.floor(time/60))
+const getSeconds = (time) => (time - getMinutes(time)*60)
+const renderTime = (time) => {
+  m = getMinutes(time);
+  s = getSeconds(time);
+  return(
+    <View style={styles.timebox}>
+      <Text style={{fontSize:40 ,fontWeight:'800'}}>
+        {m<10 ? '0'+m : m}:{s<10 ? '0'+s : s}
+      </Text>
+    </View>
+  )
+}
+
+// the main component
+// props: numberOfPlayers, duration, colors
+const ChessClocks = (props) => {
+    let n = props.numberOfPlayers
+    const [Pause,setPause] = useState(true)
+    const [aanDeBeurt,setAanDeBeurt] = useState(0)
+
+    var clocks = [];
+    for (let i = 0; i<n; i++) {
+      clocks.push(
+      <View style={{flex:1,justifyContent:'center',transform: clockStyleRotation(i)}} key={i}>
+        <TouchableOpacity
+          onPress = {() => {
+            (aanDeBeurt == i) && setAanDeBeurt( (aanDeBeurt+1) % n )
+          }}
+          >
+          <CountdownCircleTimer
+            isPlaying={ !Pause && aanDeBeurt==i }
+            duration={props.duration}
+            size={170}
+            //initialTimeRemaining={0.0020}
+            colors={props.colors[i]}
+            trailColor={pSBC(-.75,props.colors[i])}
+            >
+            {({remainingTime}) => renderTime(remainingTime)}
+          </CountdownCircleTimer>
+        </TouchableOpacity>
+      </View>)
+    }
+  
+    return(
+      <View style={{flex:1}}>
+        <View style={{flex:1}}>
+          <Button 
+            onPress ={ () => {setPause(!Pause)}}
+            title = {Pause ? "START":"PAUSE"}
+          />
+        </View>
+        <View style={{flex:12}}>
+          <View style={{flex:1,flexDirection:'row',justifyContent:'space-evenly'}}>
+            <View style={{backgroundColor: aanDeBeurt==0 ? '#80808099':'transparent'}}>{clocks[0]}</View>
+            <View style={{backgroundColor: aanDeBeurt==1 ? '#80808099':'transparent'}}>{clocks[1]}</View>
+            </View>
+          <View style={{flex:1,flexDirection:'row',justifyContent:'space-evenly'}}>
+            <View style={{backgroundColor: aanDeBeurt==3 ? '#80808099':'transparent'}}>{clocks[3]}</View>
+            <View style={{backgroundColor: aanDeBeurt==2 ? '#80808099':'transparent'}}>{clocks[2]}</View>
+          </View>
+        </View>
+      </View>
+    )
+  }
+
+const clockStyleRotation = (player) => {
+    let rot = (player == 0 || player == 3) ? "90deg" : "270deg"
+    return (
+        [
+        {rotate: rot}
+        ]
+    )
+}
+
+const styles = StyleSheet.create({
+    timebox: {
+      flex: 1,
+      padding: 0,
+      justifyContent: 'center',
+      alignItems: 'center',
+    },
+})
+  
+
+export default ChessClocks
